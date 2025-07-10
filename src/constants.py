e65707bfd1fc8bc043540d1c21bcc3a3f340c061
Dylan Blanque <dylan.blanque@gmail.com>
2025-07-10 19:26:29 -0300

    Add constants for user_shells.

---
+++ src/constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+USER_SHELL_OPTS = [
+	"/bin/sh",
+	"/bin/bash",
+	"/usr/bin/bash",
+	"/bin/rbash",
+	"/usr/bin/rbash",
+	"/bin/dash",
+	"/usr/bin/dash",
+	"/usr/bin/tmux",
+	"/usr/bin/screen",
+]
+USER_SHELL_FALLBACK = "/bin/false"
